2ebbdef3dc3f0369a4394e592a3ff85eb1fbec79
bishlilit <bishlilit@yahoo.com>
2024-07-16 19:08:21 -0400

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -1,35 +1,66 @@
 package main
 
 import (
+	// "bufio"
+	// "bytes"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	//
+
 	conn, err := l.Accept()
+	defer conn.Close()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	statusLine := "HTTP/1.1 200 OK\r\n\r\n"
+	buf := make([]byte, 1024)
+	readCount, err := conn.Read(buf)
+	if err != nil {
+		if err != io.EOF {
+			fmt.Println("unable to read from connection: ", err.Error())
+			os.Exit(1)
+		}
+	}
+
+	fmt.Println("read count: ", readCount)
+
+	readData := string(buf[:readCount])
+	fmt.Println("readData: ", readData)
+
+	lines := strings.Split(readData, "\r\n")
+	fmt.Println("lines: ", lines)
+	
+	requestTarget := strings.Split(lines[0], " ")[1]	
+
+	notFoundStatusLine := "HTTP/1.1 404 Not Found\r\n\r\n"
+	okStatusLine := "HTTP/1.1 200 OK\r\n\r\n"
+
+	var statusLine string
+	fmt.Println("request target: ", requestTarget)
+	if requestTarget == "/" {
+		statusLine = okStatusLine	
+	} else {
+		statusLine = notFoundStatusLine
+	}
 	statusLineByteArray := []byte(statusLine)
-	_, err  = conn.Write(statusLineByteArray)
+	fmt.Println("Writing statusLine to conn: ", statusLine)
+	n, err  := conn.Write(statusLineByteArray)
 	if err != nil {
 		fmt.Println("Error writing status line: ", err.Error())
 		os.Exit(1)
 	}
-	conn.Close()
+	fmt.Println("number written: ", n)
 }
